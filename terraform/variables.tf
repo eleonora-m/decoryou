d588650f12252e105e5ab37f582ff70ef504e5c5
Eleonora <devops.nora@gmail.com>
2026-03-06 14:21:41 -0500

    Add docker_image_tag variable declaration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,7 @@ variable "cluster_name" {
   type        = string
   default     = "decoryou-cluster"
 }
+variable "docker_image_tag" {
+  description = "Tag of the Docker image from Jenkins"
+  type        = string
+}
